25c3798624cd18b10a868614683f85950d5bf37b
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-01 10:56:19 +0700

    update

--- src/event.js
+++ src/event.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function Event() {
   //Create function here
+  function inputSuccess(){
+    return alert("Success input")
+  }
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <p>If you click me, alert will be appear!!</p>
+      <button onClick={()=> alert("Hello batch 37")} >Click Me</button>
+      <input onBlur={inputSuccess} type="text" />
+      {/* <button onClick={alert("Hello batch 37")} >Click Me</button> */}
+    </div>
   );
 }
 
