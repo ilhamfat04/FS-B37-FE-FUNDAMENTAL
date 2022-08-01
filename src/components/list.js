36d57b5df2cbfee54c6d70685ee0e2ae2a5a9077
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-01 11:22:15 +0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,18 @@
 // code Here
+import React from 'react'
+
+export default function List(props) {
+    // export default function List({ listName, color, qty }) {
+    let { color: listColor, qty } = props
+    if (!qty) {
+        qty = "not defined"
+    }
+
+
+
+    return (
+        <div>
+            <h1>{props.listName}- {color} - {qty}</h1>
+        </div>
+    )
+}
--- src/props.js
+++ src/props.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 //Import Components
+import List from "./components/list";
 
 function Props() {
+  const qty = 21
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <img src="https://images.unsplash.com/photo-1533473359331-0135ef1b58bf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" alt="car image" />
+      <List listName="Mercedez Benz" color="red" qty={qty} />
+      <List listName="Ferrari" color="blue" />
+    </div>
   );
 }
 
