2244c1838873817320caf7159eb975bed5b0b5e8
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-01 13:23:51 +0700

    update

--- src/conditionRendering.js
+++ src/conditionRendering.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 //Create Component PrivatePage
+function PrivatePage(props) {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <button onClick={props.logout}>Logout</button>
+    </div>
+  );
+}
 
 //Create Component GuestPage
+function GuestPage(props) {
+  return (
+    <div>
+      <h1>Please Sign</h1>
+      <button onClick={props.login}>Login</button>
+    </div>
+  );
+}
 
 function ConditionRendering() {
   //init State
+  const [isLoggedin, setIsLoggedin] = useState(false);
   return (
     // Code Inside div
-    <div>{/* conditional logic rendering */}</div>
+    <div>{isLoggedin ?
+      <PrivatePage logout={() => setIsLoggedin(!isLoggedin)} />
+      :
+      <GuestPage login={() => setIsLoggedin(!isLoggedin)} />
+    }
+
+    </div>
   );
 }
 
