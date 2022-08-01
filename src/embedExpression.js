415503b6bfee24d4da7c5f550f31a3c19ebaf869
ilham fathullah <ilhamfathullah04@gmail.com>
2022-08-01 10:33:57 +0700

    update

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,12 +2,23 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  function getMajor(){
+    return "Fullstack"
+  }
 
+  function greeting(){
+    return alert("Haiii")
+  }
   //Create a variable here
+  const companyName = "Dumbways.ID"
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h1>Welcome to {companyName} </h1>
+      <p>Hello batch 37 {getMajor()}</p>
+      <button onClick={()=>greeting}>CLick Me</button>
+    </div>
   );
 }
 
